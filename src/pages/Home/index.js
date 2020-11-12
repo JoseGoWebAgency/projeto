aaf83381c0501571673c8abe423a4c2264339a1b
Jose <josejoaquimrizzo@hotmail.com>
2020-11-12 19:31:04 +0000

    fixed home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,38 @@
 import React from 'react'
 import {View, Text, Button} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import Styles from './styles.js'
 
 export default function Home({navigation}) {
 
     return(
 
-        <View style= {Styles.containerGoWeb}>
 
-            <Text style= {Styles.textoGoWeb}>
-                Minha home de novo!
-            </Text>
+        <SafeAreaView style= {Styles.containerGoWeb}>
+            <View>
 
-            <Button
+                <Text style= {Styles.textoGoWeb}>
+                    Web App Go Web!
+                </Text>
 
-                style={Styles.btnGoWeb}
-                title = "Eventos"
-                onPress={ () => navigation.navigate('Eventos')} />
+                <Button
+                    style={Styles.btnGoWeb}
+                    title = "Criar Conta"
+                    onPress={ () => navigation.navigate('Eventos')} 
+                    color="#D0DD27"
+                />
+                    
+                <View  style={Styles.viewMargin}  />
+                <Button
+                    style={Styles.btnGoWeb}
+                    title='Iniciar sessão'
+                    color="#D0DD27"
+                    //colocar função onpress
+                />
 
-        </View>
+            </View>
 
+        </SafeAreaView>
     )
 
     
