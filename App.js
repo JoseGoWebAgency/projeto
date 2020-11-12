6c0922915ad184e529a39e8a5529c168c4a42b44
Jose <josejoaquimrizzo@hotmail.com>
2020-11-12 11:39:13 +0000

    funcional2

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack' //tem que declarar o tipo de navegação já que está separada
+import 'react-native-gesture-handler'
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack' 
 
-import Home from './src/pages/home'
-import Eventos from './src/pages/eventos'
+import Home from './src/pages/Home'
+import Eventos from './src/pages/Eventos'
+
 
 const Stack = createStackNavigator();
 
 
+
+
 export default function App() {
   return (
-    <NavigationContainer>{
-        <Stack.Navigator>
+    <NavigationContainer  >{
+        <Stack.Navigator initialRouteName="Home">
 
-          <Stack.Screen name="Home"  component={Home}/>
-          <Stack.Screen name="Eventos"  component={Eventos}/>
+          <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ title: 'Home',}}
+            />
+
+          <Stack.Screen
+            name="Eventos"
+            component={Eventos} 
+          />
 
 
         </Stack.Navigator>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import {View, Text, Button} from 'react-native'
+import styles from './styles'
 
 export default function Home({navigation}) {
 
     return(
 
-        <View>
+        <View style= {styles.containerGoWeb}>
 
-            <Text>
+            <Text style= {styles.textoGoWeb}>
                 Minha home de novo!
             </Text>
 
             <Button
+
+                style={styles.btnGoWeb}
                 title = "Eventos"
-                onPress={ () => navigation.navigate('Eventos')} >
-            </Button>
+                onPress={ () => navigation.navigate('Eventos')} />
 
         </View>
 
     )
+
+    
 }
