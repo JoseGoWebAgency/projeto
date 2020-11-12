9fe86895f92b6c044948c6aa4727fd8cc6cf46a6
Jose <josejoaquimrizzo@hotmail.com>
2020-11-12 16:17:14 +0000

    fixed design

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import {View, Text, Button} from 'react-native'
-import styles from './styles'
+import Styles from './styles.js'
 
 export default function Home({navigation}) {
 
     return(
 
-        <View style= {styles.containerGoWeb}>
+        <View style= {Styles.containerGoWeb}>
 
-            <Text style= {styles.textoGoWeb}>
+            <Text style= {Styles.textoGoWeb}>
                 Minha home de novo!
             </Text>
 
             <Button
 
-                style={styles.btnGoWeb}
+                style={Styles.btnGoWeb}
                 title = "Eventos"
                 onPress={ () => navigation.navigate('Eventos')} />
 
