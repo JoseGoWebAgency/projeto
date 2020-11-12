e6ea441cab4c452e8aae8e0b23676bf697805156
Jose <josejoaquimrizzo@hotmail.com>
2020-11-12 19:40:24 +0000

    com estilos

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+    containerGoWeb:{
+        flex:1,
+        backgroundColor:'#666666',
+        alignItems:"center",
+        justifyContent:'center',
+
+    },
+
+    textoGoWeb:{
+        fontSize:20,
+        textAlign:"center",
+        margin:10,
+        color: '#D0DD27'
+
+    },
+
+    btnGoWeb:{
+        backgroundColor:"#D0DD27",
+        margin:10
+    
+    },
+
+    viewMargin:{
+        margin:10,
+        backgroundColor:'#666666',
+        
+
+    }
+})
